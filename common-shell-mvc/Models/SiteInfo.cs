c7ca11403173fefd88947696e02ff293b5efc6cf
sumanvl <subramaniyanvlu@gmail.com>
2026-05-22 21:09:30 +0530

    Add files via upload

---
+++ common-shell-mvc/Models/SiteInfo.cs
@@ -0,0 +1,14 @@
+namespace ACCT.Common;
+
+public static class SiteInfo
+{
+    public const string CompanyName = "JNS Accounting Solutions";
+    public const string ShortName = "JNS Accounting";
+    public const string Tagline = "Tax • Accounts • Compliance";
+    public const string Phone = "+91 93639 93765";
+    public const string AlternatePhone = "+91 96266 33266";
+    public const string Email = "admin@jnsaccounting.com";
+    public const string Address = "83 PV Krishnan Street, KK Pudur, Coimbatore - 641 038";
+    public const string WhatsAppUrl = "https://wa.me/919363993765";
+    public const string InstagramUrl = "https://www.instagram.com/jnsaccountingsolutions/";
+}
